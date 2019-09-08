c69d0c21ad01eb2cb84f7b510689db85631ad774
Dongho Park <hue.dongho.park@gmail.com>
2019-09-08 15:17:06 +1200

    Updates

---
+++ precache-manifest.22129ccd614376863832af7adace0481.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57dea16b1d0d610e74436c679cf0406",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76b695ba9fb080d0d196",
+    "url": "/static/css/main.08e3d51a.chunk.css"
+  },
+  {
+    "revision": "f5576e6c26cfe2dce255",
+    "url": "/static/js/2.079ff218.chunk.js"
+  },
+  {
+    "revision": "76b695ba9fb080d0d196",
+    "url": "/static/js/main.dda26f2d.chunk.js"
+  },
+  {
+    "revision": "37357a75079c0d47141c",
+    "url": "/static/js/runtime~main.34813451.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c6fc13114adc8bd7422e1b37adb1320.js"
+  "/precache-manifest.22129ccd614376863832af7adace0481.js"
 );
 
 self.addEventListener('message', (event) => {
